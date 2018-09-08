0993a232b3dd15895044df20aa29569d2e3efe7c
The Singer <kayla312@harbornet.com>
2018-09-08 16:20:13 -0700

    basic directions fopr what i need to create! onto PSUDOCODING

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+// PSUDO CODE FOR TRIVIAL GAME
+// THEME: MUSIC ( OR THERE ABOUTS....)
+
+// DIRECTIONS
+
+// 3 rounds of 10 question (ambitious but I’m gonna try)
+// Multiple choice or true false for options to answer
+// ~make sure user can only select one answer for each question
+// 30-60 seconds per question, so countdown clock should limit the game length
+// ~when timer runs down, reveal score of game (correct and incorrect)
+
