a6ff08b932515c8aa74323ace00bd168bf70df44
The Singer <kayla312@harbornet.com>
2018-09-08 17:28:38 -0700

    jk. forgot to save. trying again

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,7 +18,7 @@
 // 	if answer selected is incorrect, add 1++ to score incorrect
 // 	if there are answers un-answered when the time runs out, 1++ unanswered
 
-//  Have a timer set up to count down from either 5 min or 10 min (depending on how hard the questions are)
+//  Have a timer set up to count down (depending on how hard the questions are the time length)
 // 	show an alert at 30 second left to warn the player
 // 	when the timer runs out,  calculate the accurate scores for correct, incorrect and unanswered
 // 	display the score on the page for the player to review at the end of the game 
