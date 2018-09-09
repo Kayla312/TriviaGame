707a2833f51d1f7c97756e2ef40c76ee30a3c206
The Singer <kayla312@harbornet.com>
2018-09-08 17:27:53 -0700

    added some basic psudo code as I am iimagining it now...hopefully ive got the right idea

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,3 +9,25 @@
 // 30-60 seconds per question, so countdown clock should limit the game length
 // ~when timer runs down, reveal score of game (correct and incorrect)
 
+// Create a form with 10 questions
+// 	each question should have options (true/false or ABCD)
+
+// 	EACH QUESTION SHOULD ONLY BE ABLE TO ANSWER ONCE!
+// ______at the end when the whole thing is either submitted or times out _____
+// 	if the answer selected is correct, add 1++ to score correct
+// 	if answer selected is incorrect, add 1++ to score incorrect
+// 	if there are answers un-answered when the time runs out, 1++ unanswered
+
+//  Have a timer set up to count down from either 5 min or 10 min (depending on how hard the questions are)
+// 	show an alert at 30 second left to warn the player
+// 	when the timer runs out,  calculate the accurate scores for correct, incorrect and unanswered
+// 	display the score on the page for the player to review at the end of the game 
+
+// ****notes on the video examples****
+// Background doesn’t change
+// Header/title doesn’t change
+// Just information in the middle of the page changes (aka start button/questions/review of answers)
+// 		~~could be done using nested containers
+// 		~~so that as each thing happens (on click for start button—>changes main container…after answering all the questions or timer running out(alert about timer running down)—>changes main container…
+
+	
